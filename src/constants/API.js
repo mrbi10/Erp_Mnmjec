e3d9fcdde02f482f3b41731907c3087d0cc509de
mrbi10 <hetmyerwi@gmail.com>
2025-11-11 13:48:04 +0530

    pre-fianl

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,10 +1,10 @@
-//college
-export const BASE_URL = "http://192.168.0.25:5000/api";
-//localhost
+// college
+// export const BASE_URL = "http://192.168.0.25:5000/api";
+// localhost
 // export const BASE_URL = "http://localhost:5000/api";
-//Anna home
+// Anna home
 // export const BASE_URL = "http://192.168.1.8:5000/api";
-//home
+// home
 // export const BASE_URL = "http://192.168.29.235:5000/api";
-//pati home
-// export const BASE_URL = "http://192.168.1.5:5000/api";
+// pati home
+export const BASE_URL = "http://192.168.1.11:5000/api";
