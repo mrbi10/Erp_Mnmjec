c1e7fbe7a5336a5c0c5217ff00810626852135a3
mrbi10 <hetmyerwi@gmail.com>
2025-11-16 20:20:17 +0530

    api change

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,11 +1,11 @@
 // college
 // export const BASE_URL = "http://192.168.0.25:5000/api";
 // localhost
-export const BASE_URL = "http://localhost:5000/api";
+// export const BASE_URL = "http://localhost:5000/api";
 // Anna home
 //export const BASE_URL = "http://192.168.1.7:5000/api";
 // home
-// export const BASE_URL = "http://192.168.29.236:5000/api";
+export const BASE_URL = "http://192.168.29.236:5000/api";
 // pati home
 // export const BASE_URL = "http://192.168.1.11:5000/api";
 // Mrbi hotspot
